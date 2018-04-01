8030f22bf28263ae8e2b6e2fc7c860214ed86136
jan nietfeld <jan@meltwater.com>
2018-04-01 16:01:40 +0200

    Add apollo (not working).

--- web/static/js/autocomplete.js
+++ web/static/js/autocomplete.js
@@ -4,12 +4,32 @@ import Autocomplete from 'v-autocomplete'
 
 import App from './components/app.vue'
 
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+
+
+const httpLink = new HttpLink({
+    // You should use an absolute URL here
+    uri: 'http://localhost:4000/graphql-api',
+})
+
+// Create the apollo client
+const apolloClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+})
+
+// Install the vue plugin
 Vue.use(Autocomplete)
+Vue.use(VueApollo)
 
 document.onreadystatechange = function () {
     if (document.readyState == "complete") {
         new Vue({
-            // provide: apolloProvider.provide(),
+            provide: apolloProvider.provide(),
             render: createEle => createEle(App)
         }).$mount('#app')
     }
